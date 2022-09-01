59cdb3024d425418cb20feb24f2e2c85a7b5895c
Dennis Lee <cobays@gmail.com>
2022-09-01 17:36:10 +0900

    Fixed many many many stuff.

--- src/app/code/local/Cobay/OnlineCustomersGeo/Block/Widget/Grid/Column/Renderer/Ipcountry.php
+++ src/app/code/local/Cobay/OnlineCustomersGeo/Block/Widget/Grid/Column/Renderer/Ipcountry.php
@@ -7,17 +7,9 @@ class Cobay_OnlineCustomersGeo_Block_Widget_Grid_Column_Renderer_Ipcountry
 
 		// geoip_contry_code, set by same variable name both Varnish and Nginx
 		$countryCode = $_SERVER["HTTP_GEOIP_COUNTRY_CODE"] ?? null; // ?? php7 Null Coalesce Operator
-		if($countryCode===null){
-			$remote_addr = $row->getRemoteAddr();
-			$geoIP = Mage::getSingleton('geoip/country'); // for Sandfox_GeoIP module
-			$countryCode = $geoIP->getCountryByIp( long2ip($remote_addr) );  // for Sandfox_GeoIP module
-		}
 		if(empty($countryCode)) return $retour;
 
 		$countryName = $_SERVER["HTTP_GEOIP_COUNTRY_NAME"] ?? null; // $username = isset($_GET['user']) ? $_GET['user'] : 'nobody';
-		if($countryName===null){
-			$countryName = Cobay_Common_Helper_Geo::$countries[$countryCode];
-		}
 		if(empty($countryCode)) return $retour;
 
 		$image = $this->getSkinUrl('images/onlinecustomersgeo/' . strtolower($countryCode) . '.gif');
